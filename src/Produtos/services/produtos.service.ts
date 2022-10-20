435f0a3501f46e03b710743fa5ff7ac3f84c854b
DanArtt <xdogman5@gmail.com>
2022-10-20 17:02:46 -0300

    incrementacao do commit

--- src/Produtos/services/produtos.service.ts
+++ src/Produtos/services/produtos.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@nestjs/common"
+import {Injectable, HttpException, HttpStatus} from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
 import { Repository, ILike, DeleteResult } from "typeorm"
 import { Produto } from "../entities/produtos.entity"
@@ -24,6 +24,46 @@ export class ProdutosService {
                 id
             }
         })
+    if (!produtos) {
+        throw new HttpException('ID referente ao produto não existe.', HttpStatus.NOT_FOUND);
+    }
+    return produtos;
+    }
+
+//---------------------Metodo Achar pelo nome-------------------------
+
+    async findByName(nome: string): Promise<Produto[]> {
+        return await this.produtosRepository.find({
+            where: {
+                nome: ILike(`%${nome}%`)
+            }
+        })
+    }
+
+//---------------------Mandar Todos os Dados para o Bando de Dados-----------------------
+
+    async create(produtos: Produto): Promise<Produto> {
+        return await this.produtosRepository.save(produtos);
+    }
+
+//---------------------Metodo de Atualizar Dados no Banco de Dados pelo ID-----------------------
+
+    async update(produtos: Produto): Promise<Produto> {
+        let buscarProdutos: Produto = await this.findById(produtos.id)
+        if (!buscarProdutos || !produtos.id) {
+            throw new HttpException(`ID referente ao produto não existe.`, HttpStatus.NOT_FOUND);
+        }
+        return await this.produtosRepository.save(produtos);
+    }
+
+//---------------------Metodo de Deletar Dados do Banco de Dados pelo ID-------------------------
+
+    async delete(id: number): Promise<DeleteResult> {
+        let buscarProdutos: Produto = await this.findById(id);
+        if (!buscarProdutos) {
+            throw new HttpException(`ID referente ao produto não existe.`, HttpStatus.NOT_FOUND);
+        }
+        return await this.produtosRepository.delete(id);
     }
 }
 
